7f3388822e19c71a7cb00361f115da8e98dd183d
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2023-11-28 14:10:27 -0800

    new function added

--- rent_project.py
+++ rent_project.py
@@ -6,34 +6,66 @@
 # Contract = 1 month or 1 year that is store in list etc
 print("Rent Management System Program".center(75, " "))
 ###################################################
-def create_tenant(name, contact, unit=None, duration=None):
-    return {"name": name, "contact": contact, "contracts": [{"unit": unit, "duration": duration}]}
-# Example usage, unit = str, duration = int
-# tenant_info = create_tenant("John Doe", "+123456789", unit="A101", duration=12, contract=[5, "month/year"])
+def create_tenant(name, contact, unit, duration):
+    return {"name": name, "contact": contact, "property": None, "duration": duration, "unit": unit}
 
 def create_property(name, rent):
-    return {"name": name, "rent": rent, "tenant": None}
+    return {"name": name, "rent": rent}
 
 def assign_tenant_to_property(tenant, property, duration, unit):
-    tenant["contracts"].append({"property": property, "duration": duration, "unit": unit})
+    tenant["property"] = property
+    tenant["duration"] = duration
+    tenant["unit"] = unit
     property["tenant"] = tenant
-
+    
 
 def display_properties(properties):
     print("\nProperty Details:")
     for property in properties:
-        tenant_info = property["tenant"]["name"] if property["tenant"] else "Vacant"
-        print(f"Property: {property['name']}, Rent: {property['rent']}, Tenant: {tenant_info}")
+        print(f"Property: {property['name']}, Rent: {property['rent']}")   ##, Tenant: {tenant_info}")
 
-def display_tenants(tenants):
-    print("\nTenant Details:\n")
-    for tenant_index, tenant in enumerate(tenants, start=1):
-        print(f"Tenant {tenant_index}:\n   Name: {tenant['name']}, Contact: {tenant['contact']}, Contracts: {len(tenant['contracts'])}")
-        for contract_index, contract in enumerate(tenant['contracts'], start=1):
-            print(f"   Contract {contract_index}: Property: {contract['property']['name']}, Duration: {contract['duration']} {contract['unit']}")
+
+# Two New Functions:
+def connect_tenants_and_properties(tenants, properties):
+    for i in range(min(len(tenants), len(properties))):
+        assign_tenant_to_property(tenants[i], properties[i], tenants[i]["duration"], tenants[i]["unit"])
+
+    if len(tenants) > len(properties):
+        print("Warning: More tenants than properties. You can add new properties.")
+        add_property_choice = input("Do you want to add a new property? (y/n): ").lower()
+
+        if add_property_choice == 'y':
+            # Add new property
+            property_name = input("Enter the name of the new property: ").title()
+            rent = int(input("Enter the rent amount for the new property: "))
+            new_property = create_property(property_name, rent)
+            properties.append(new_property)
+
+            # Assign the new property to the remaining tenants
+            for i in range(len(properties), len(tenants)):
+                tenants[i]["property"] = new_property
+                tenants[i]["duration"] = tenants[i]["duration"]
+                tenants[i]["unit"] = tenants[i]["unit"]
+                new_property["tenant"] = tenants[i]
+
+            print(f"New property '{property_name}' added successfully.")
+
+    print("\nAssigned Tenants and Properties:")
+    for i, tenant in enumerate(tenants, start=1):
+        property_name = tenant['property']['name'] if tenant['property'] else "N/A"
+        duration = tenant['duration'] if tenant['duration'] else "N/A"
+        unit = tenant['unit'] if tenant['unit'] else "N/A"
+        print(f"Tenant {i}: Name: {tenant['name']}, Property: {property_name}, Duration: {duration} {unit}")
+
+def display_tenants_properly(tenants):
+    print("\nTenant Details:")
+    for tenant in tenants:
+        print(f"\nName: {tenant['name']}, Contact: {tenant['contact']}, Duration: {tenant['duration']} {tenant['unit']}")
+
+
+
 # list of Tenant to add:
 tenants = [
-    ##########################################
     create_tenant("Person1", "+92321456789", unit="month", duration=6),
     create_tenant("Person2", "+92322567890", unit="year", duration=2),
     create_tenant("Person3", "+92323678901", unit="month", duration=3),
@@ -47,52 +79,85 @@ properties = [
     create_property("House4", 13000)
 ]
 
+def get_user_choice():
+    while True:
+        try:
+            choice = int(input('Enter The Option:\n1) Add New Tenant\n2) Remove Existing Tenant\n3) Show All Tenants\n4) Show All Properties\n5) Show Tenant With Their Property\n6) Add new Property\n-> Enter the Option: '))
+            return choice
+        except ValueError as e:
+            print(f"Error: {e}")
+            print("Please enter a valid integer.")
 
-for i in range(len(properties)):
-    duration = int(input(f"Enter the contract duration for {properties[i]['name']}: "))
-    unit = input(f"Enter the unit (month/year) for {properties[i]['name']}: ").lower()
-    assign_tenant_to_property(tenants[i], properties[i], duration, unit)
-
-# display_tenants(tenants)
-# display_properties(properties)
 
 while True:
-    try:
-        choice = int(input('Enter The Option:\n1) Add New Tenant\n2) Remove Existing Tenant\n3) Show All Tenants\n'))
-    except ValueError as e:
-        print(f"Error: {e}")
-        choice = int(input('Enter The Option:\n1) Add New Tenant\n2) Remove Existing Tenant\n3) Show All Tenants\n'))
-
+    # Var of a function:
+    choice = get_user_choice()
+    
     if choice == 1:
         # Add new tenant
         name = input("Enter The name of New Tenant: ").title()
         contact = input("Enter the Contact Number with Country Code: ")
-        ################################
         try:
             duration = int(input("Enter The Duration (Time) in num: "))
         except ValueError as e:
             print(e)
             duration = input("Enter The Duration (Time) in num: ")
+            if 0 < duration < 12:
+                pass
+            else:
+                print()
+        except Exception as e:
+            print(e)
+            duration = input("Enter The Duration (Time) in num: ")
             
         unit = input("Enter The Time Period (Month/year): ").lower()
+        # New Tenant Created 
         new = create_tenant(name, contact, unit, duration)
         tenants.append(new)
         print(f"Tenant {name} added successfully.")
+        if len(properties) > len(tenants):
+            print("The number of properties is already greater than the number of tenants.")
+        else:
+            # Add New Property
+            name = input("Enter The name of New Tenant: ").title()
+            rent = int(input("Enter The Rent Amount: "))
+            new_property = create_property(name, rent)
+            properties.append(new_property)
+            print("Property added successfully.")
+        pass
+    
     elif choice == 2:
+    # Remove Existing Tenant
         name = input("Enter The name of Tenant to remove: ").title()
         for tenant in tenants:
             if tenant["name"] == name:
                 tenants.remove(tenant)
                 print(f"Tenant {name} removed successfully.")
                 break
-            else:
-                print(f"Tenant {name} not found.")
-        # Remove Existing Tenant
+        else:
+            print(f"Tenant {name} not found.")
+
     elif choice == 3:
-        display_tenants(tenants)
+        display_tenants_properly(tenants)
         
-        # Show All Tenants
-        pass
+    elif choice == 4:
+        display_properties(properties)
+        
+    elif choice == 5:
+        # 
+        # print("\nAssigned Tenants and Properties:")
+        # for i, tenant in enumerate(tenants):
+        #     print(f"Tenant {i}: Name: {tenant['name']}, Property: {tenant['property']}, Duration: {tenant['duration']}")
+        #
+        a = connect_tenants_and_properties(tenants, properties)
+        print(a)
+    
+    elif choice == 6:
+        name = input("Enter The Properties Name: ")
+        rent = int(input("Enter the Rent Amount: "))
+        cp = create_property(name, rent)
+        properties.append(cp)
+
     else:
         break  # Exit the loop if the choice is not 1, 2, or 3
 
