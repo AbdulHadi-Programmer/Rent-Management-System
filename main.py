2b2435422887de423a42f1e4a4522f774fc248ef
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2023-12-03 12:05:35 -0800

    Write lease class and other method of tenant and lease class

--- main.py
+++ main.py
@@ -21,15 +21,15 @@ p2 = Property("House2", 4, 25000)
 p2.Detail()
 properties = [p1, p2]
 
-# properties = [
-#         Property("House1", 2, 15000),
-#         Property("House1", 3, 52000),
-#         Property("House1", 2, 12000),
-#         Property("House2", 1, 10000),
-#         Property("House2", 4, 60000),
-#         Property("House2", 5, 65000),
-#         # Add more properties as needed
-#     ]
+properties = [
+        Property("House1", 2, 15000),
+        Property("House1", 3, 52000),
+        Property("House1", 2, 12000),
+        Property("House2", 1, 10000),
+        Property("House2", 4, 60000),
+        Property("House2", 5, 65000),
+        # Add more properties as needed
+    ]
 #     # Display details for each property using a loop and enumerate
 # for index, property_obj in enumerate(properties, start=1):
 #     print(f"\nProperty {index} Details:")
@@ -46,18 +46,64 @@ class Tenant:
         # print(f'Tenant Detail:\nName: {self.name}\nGender: {self.gender}\nPhone Num: {self.contactNum}\nEmail: {self.email}\n')
         print(f'Name: {self.name}\nGender: {self.gender}\nPhone Num: {self.contactNum}\nEmail: {self.email}\n')
     
-    # def contract(self):
-    #     print(f'The Contract of {self.name} is:\nDuration: {self.duration}\n')
+    def update_details(self, new_name, new_gender, new_contactNum, new_email):
+        self.name = new_name
+        self.gender = new_gender
+        self.contactNum = new_contactNum
+        self.email = new_email
+        print(f'Tenant details updated.\n')
 
-t1 = Tenant("Abdul Hadi", "Male", "+922319536","abc@gmail.com", "1 year")
-t2 = Tenant("Jane Smith", "Female", "+987654321", "jane@example.com", "4 month")
-t3 = Tenant("Bob Johnson", "Male", "+111222333", "bob@example.com", "2 years")
-t4 = Tenant("John Doe", "Male", "+123456789", "john@example.com", "6 months")
+
+t1 = Tenant("Abdul Hadi", "Male", "+922319536","abc@gmail.com")
+t2 = Tenant("Jane Smith", "Female", "+987654321", "jane@example.com")
+t3 = Tenant("Bob Johnson", "Male", "+111222333", "bob@example.com")
+t4 = Tenant("John Doe", "Male", "+123456789", "john@example.com")
 tenants = [t1, t2, t3, t4]
 # t1.person_detail()
 # t1.contract()
 for index, tenant in enumerate(tenants, start=1):
     print(f"\nTenant {index} Details:")
     tenant.person_detail()
-    tenant.contract()
+    
+# Write a Lease Class to connect tenant with property:
+class Lease:
+    def __init__(self, tenant, property, duration):
+        self.tenant = tenant
+        self.property = property
+        self.duration = duration
+        
+    def display_details(self):
+        print(f'Lease Details for {self.tenant.name}:\nProperty: {self.property.HouseName}\n')  # Fix the typo here
 
+    def update_duration(self, new_duration):
+        self.duration = new_duration
+        print(f'Duration for {self.tenant.name} updated to {self.duration}\n')
+
+    def remove_tenant(self):
+        print(f'Removing tenant {self.tenant.name} from the lease.\n')
+        self.tenant = None  # Setting tenant to None indicates that the lease is now vacant
+# l1 = Lease(tenants[0], properties[0])
+# l2 = Lease(tenants[1], properties[1])
+# l3 = Lease(tenants[2], properties[2])
+#################################################
+print("\n\n\nExample Showeing:\n\n")
+# Example usage:
+property1 = Property("House1", 2, 12000)
+tenant1 = Tenant("Abdul Hadi", "Male", "+922319536", "abc@gmail.com")
+lease1 = Lease(tenant1, property1, "1 year")
+
+# Display details before any changes
+lease1.display_details()
+
+# Update tenant details
+tenant1.update_details("Updated Abdul Hadi", "Male", "+922319537", "updated_abc@gmail.com")
+
+# Update lease duration
+lease1.update_duration("2 years")
+
+# Display details after updates
+lease1.display_details()
+
+# Remove tenant from the lease
+lease1.remove_tenant()
+lease1.display_details()
