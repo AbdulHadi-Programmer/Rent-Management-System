d427cb3c070eb29379c75d74c9c96d8a47451fb8
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2023-12-03 11:45:53 -0800

    write tenant class and methods

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Write two list of tenants and properties
 # Property is a class
 # properties is a list 
+
 class Property:
     Location = "Gazderabad" # I make location a class var because my all properties belong to this area
     def __init__(self, HouseName, noOfRooms, rent):
@@ -34,4 +35,29 @@ properties = [p1, p2]
 #     print(f"\nProperty {index} Details:")
 #     property_obj.Detail()
 
- 
+class Tenant:
+    def __init__(self, name, gender, contactNum, email):
+        self.name = name
+        self.gender = gender
+        self.contactNum = contactNum
+        self.email = email
+        
+    def person_detail(self):
+        # print(f'Tenant Detail:\nName: {self.name}\nGender: {self.gender}\nPhone Num: {self.contactNum}\nEmail: {self.email}\n')
+        print(f'Name: {self.name}\nGender: {self.gender}\nPhone Num: {self.contactNum}\nEmail: {self.email}\n')
+    
+    # def contract(self):
+    #     print(f'The Contract of {self.name} is:\nDuration: {self.duration}\n')
+
+t1 = Tenant("Abdul Hadi", "Male", "+922319536","abc@gmail.com", "1 year")
+t2 = Tenant("Jane Smith", "Female", "+987654321", "jane@example.com", "4 month")
+t3 = Tenant("Bob Johnson", "Male", "+111222333", "bob@example.com", "2 years")
+t4 = Tenant("John Doe", "Male", "+123456789", "john@example.com", "6 months")
+tenants = [t1, t2, t3, t4]
+# t1.person_detail()
+# t1.contract()
+for index, tenant in enumerate(tenants, start=1):
+    print(f"\nTenant {index} Details:")
+    tenant.person_detail()
+    tenant.contract()
+
